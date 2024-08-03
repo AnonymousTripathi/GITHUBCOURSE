293c7d2cc0139933659608b4ec618129ce7e7c24
HarshTripathi <tripathiharsh227@gmail.com>
2024-08-03 17:56:59 +0530

    added MUL.java

---
+++ DIFF.java
@@ -0,0 +1,7 @@
+public class diff{
+    public static void main (String [] args){
+        int a =3;
+        int b=9;
+        System.out.println(b-a);
+    }
+}
---
+++ MUL.java
@@ -0,0 +1,4 @@
+public class Mul{
+    int a=8,b=5;
+    System.out.println(a*b);
+}
